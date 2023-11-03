8a631c6ef322c78594b252bf3057cc4bb648d989
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-11-03 13:03:07 +0100

    Practicing Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './components/server/server.component';
 import { ServersComponent } from './components/servers/servers.component';
+import { WarningalertComponent } from './components/warningalert/warningalert.component';
+import { SuccessalertComponent } from './components/successalert/successalert.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent, WarningalertComponent, SuccessalertComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
