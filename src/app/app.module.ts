6d6980bf31c32f7961090b9582078effa5c1df2b
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-11-04 17:57:39 +0100

    Setting Up the Application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,9 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './components/server/server.component';
-import { ServersComponent } from './components/servers/servers.component';
-import { WarningalertComponent } from './components/warningalert/warningalert.component';
-import { SuccessalertComponent } from './components/successalert/successalert.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent, WarningalertComponent, SuccessalertComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
