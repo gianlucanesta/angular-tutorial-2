7a19c2fc4180b8a1c6dadf45a6187e071e34efe2
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-11-07 12:55:25 +0100

    Closing the Dropdown From Anywhere

--- src/app/components/shared/dropdown.directive.ts
+++ src/app/components/shared/dropdown.directive.ts
@@ -3,36 +3,18 @@ import {
   ElementRef,
   HostBinding,
   HostListener,
-  Renderer2,
 } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]',
 })
 export class DropdownDirective {
-  // @HostBinding('class.open') isOpen = false;
+  constructor(private el: ElementRef) {}
 
-  // @HostListener('click') toggleOpen() {
-  //   this.isOpen = !this.isOpen;
-  // }
-
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
-
-  @HostListener('click') onClick() {
-    const dropdown = this.el.nativeElement.nextElementSibling;
-
-    if (dropdown) {
-      this.renderer.addClass(dropdown, 'show');
-    }
-  }
-
-  @HostListener('document:click', ['$event.target']) onDocumentClick(
-    target: any
-  ) {
-    const dropdown = this.el.nativeElement.nextElementSibling;
-
-    if (dropdown && !dropdown.contains(target)) {
-      this.renderer.removeClass(dropdown, 'show');
-    }
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.el.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
   }
 }
