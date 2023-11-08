2c1d27e49ec011ae0c6187965132de11b24b86d3
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-11-08 16:19:51 +0100

    Managing Recipes in a Recipe Service

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,26 +10,12 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is a simple description',
-      'https://www.bibbyskitchenat36.com/wp-content/uploads/2021/01/DSC_9104-1.jpg'
-    ),
-    new Recipe(
-      'Another Test Recipe',
-      'This is a simple description',
-      'https://www.bibbyskitchenat36.com/wp-content/uploads/2021/01/DSC_9104-1.jpg'
-    ),
-    new Recipe(
-      'Another One Test Recipe',
-      'This is a simple description',
-      'https://www.bibbyskitchenat36.com/wp-content/uploads/2021/01/DSC_9104-1.jpg'
-    ),
-  ];
+  recipes!: Recipe[];
 
-  constructor() {}
-  ngOnInit(): void {}
+  constructor(private reciperService: RecipeService) {}
+  ngOnInit(): void {
+    this.recipes = this.reciperService.getRecipes();
+  }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
--- src/app/components/recipes/recipe.service.ts
+++ src/app/components/recipes/recipe.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeService {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Hamburger di carne',
+      'This is a simple description',
+      'https://www.cucchiaio.it/content/cucchiaio/it/ricette/2018/08/hamburger-di-carne/_jcr_content/header-par/image-single.img.jpg/1533820219744.jpg'
+    ),
+    new Recipe(
+      'Lasagne alla bolognese',
+      'This is a simple description',
+      'https://www.cucchiaio.it/content/cucchiaio/it/ricette/2009/12/ricetta-lasagne-bolognese/_jcr_content/header-par/image_single.img.jpg/1462958827968.jpg'
+    ),
+    new Recipe(
+      'Merluzzo Gratinato',
+      'This is a simple description',
+      'https://www.cucchiaio.it/content/cucchiaio/it/ricette/2018/02/merluzzo-gratinato/_jcr_content/header-par/image-single.img.jpg/1519903977793.jpg'
+    ),
+  ];
+  constructor() {}
 
-  constructor() { }
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
