8ae4bbc171de0edfaafd5b1fe788040319b2976c
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-11-12 19:21:57 +0100

    Analyzing Angular Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  styles: [
-    `
-      h3 {
-        color: dodgerblue;
-      }
-    `,
-  ],
 })
-export class AppComponent {
-  serverElements = [
-    { type: 'server', name: 'Testserver', content: 'Just a test!' },
-  ];
-
-  onServerAdded(serverData: { serverName: string; serverContent: string }) {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent,
-    });
-  }
-
-  onBlueprintAdded(blueprintData: {
-    serverName: string;
-    serverContent: string;
-  }) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: blueprintData.serverName,
-      content: blueprintData.serverContent,
-    });
-  }
-
-  onChangeFirst() {
-    this.serverElements[0].name = 'Changed!';
-  }
-
-  onDestroyFirst() {
-    this.serverElements.splice(0, 1);
-  }
+export class AppComponent implements OnInit {
+  ngOnInit(): void {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,9 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './components/server/server.component';
-import { ServersComponent } from './components/servers/servers.component';
-import { WarningalertComponent } from './components/warningalert/warningalert.component';
-import { SuccessalertComponent } from './components/successalert/successalert.component';
-import { CockpitComponent } from './components/cockpit/cockpit.component';
-import { ServerElementComponent } from './components/server-element/server-element.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningalertComponent,
-    SuccessalertComponent,
-    CockpitComponent,
-    ServerElementComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
