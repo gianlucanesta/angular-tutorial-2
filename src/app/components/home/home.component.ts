5deb66c01568b9893b5884302ae0a52af59414d8
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-11-12 21:05:03 +0100

    Errors & Completion

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -18,6 +18,12 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count === 5) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater 3!'));
+        }
         count++;
       }, 1000);
     });
@@ -25,6 +31,13 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.firstObsSubscription = customIntervalObservable.subscribe(
       (data: any) => {
         console.log(data);
+      },
+      (error: any) => {
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        console.log('Completed!');
       }
     );
   }
