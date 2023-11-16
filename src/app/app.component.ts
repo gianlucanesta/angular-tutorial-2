11f72e46a437a514267c249dfc320939c36aa323
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-11-16 22:44:36 +0100

    Reactive: Creating Custom Validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,10 +16,15 @@ export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm!: FormGroup;
 
+  forbiddenUsernames = ['Chris', 'Anna'];
+
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl(null, Validators.required),
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this),
+        ]),
         email: new FormControl(null, [Validators.required, Validators.email]),
       }),
       gender: new FormControl('male'),
@@ -44,5 +49,12 @@ export class AppComponent implements OnInit {
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (this.signupForm.get('hobbies') as FormArray).push(control);
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } | null {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return { nameIsForbidden: true };
+    }
+    return null;
   }
 }
