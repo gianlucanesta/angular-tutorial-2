e5039a3fd2b07e483036837ed9cbfa0f6a0295c0
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-11-19 22:08:37 +0100

    Creating the Template for the (Reactive) Recipe Edit Form

--- src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -53,6 +53,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
