56bc8ae0db672f9fa2b37a73495c72904063b82a
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-11-21 22:12:22 +0100

    Setup App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  styles: [
-    `
-      h3 {
-        color: dodgerblue;
-      }
-    `,
-  ],
 })
-export class AppComponent {
-  serverElements = [
-    { type: 'server', name: 'Testserver', content: 'Just a test!' },
-  ];
+export class AppComponent implements OnInit {
+  loadedPosts = [];
 
-  onServerAdded(serverData: { serverName: string; serverContent: string }) {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent,
-    });
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {}
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    console.log(postData);
   }
 
-  onBlueprintAdded(blueprintData: {
-    serverName: string;
-    serverContent: string;
-  }) {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: blueprintData.serverName,
-      content: blueprintData.serverContent,
-    });
+  onFetchPosts() {
+    // Send Http request
   }
 
-  onChangeFirst() {
-    this.serverElements[0].name = 'Changed!';
-  }
-
-  onDestroyFirst() {
-    this.serverElements.splice(0, 1);
+  onClearPosts() {
+    // Send Http request
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './components/server/server.component';
-import { ServersComponent } from './components/servers/servers.component';
-import { WarningalertComponent } from './components/warningalert/warningalert.component';
-import { SuccessalertComponent } from './components/successalert/successalert.component';
-import { CockpitComponent } from './components/cockpit/cockpit.component';
-import { ServerElementComponent } from './components/server-element/server-element.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningalertComponent,
-    SuccessalertComponent,
-    CockpitComponent,
-    ServerElementComponent,
-  ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
