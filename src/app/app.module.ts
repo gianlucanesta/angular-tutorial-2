f6dab558b7ddd1434a8dc537c79da40bafcfa266
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-11-21 12:01:56 +0100

    Creating a Custom Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './components/server/server.component';
-import { ServersComponent } from './components/servers/servers.component';
-import { WarningalertComponent } from './components/warningalert/warningalert.component';
-import { SuccessalertComponent } from './components/successalert/successalert.component';
-import { CockpitComponent } from './components/cockpit/cockpit.component';
-import { ServerElementComponent } from './components/server-element/server-element.component';
+import { ShortenPipe } from './pipes/shorten.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningalertComponent,
-    SuccessalertComponent,
-    CockpitComponent,
-    ServerElementComponent,
-  ],
+  declarations: [AppComponent, ShortenPipe],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
