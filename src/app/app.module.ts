3b42a438996dea0e4ef2a3d1e32217be53ca4efa
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-11-21 17:27:06 +0100

    Exercise 8 assignment solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShortenPipe } from './pipes/shorten.pipe';
 import { FilterPipe } from './pipes/filter.pipe';
+import { ReversePipe } from './pipes/reverse.pipe';
+import { SortPipe } from './pipes/sort.pipe';
 
 @NgModule({
-  declarations: [AppComponent, ShortenPipe, FilterPipe],
+  declarations: [AppComponent, ShortenPipe, FilterPipe, ReversePipe, SortPipe],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
