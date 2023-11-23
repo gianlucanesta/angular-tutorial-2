4f261860919ce592469cab348956f144597ab33b
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-11-23 21:45:19 +0100

    Response Interceptors

--- src/app/services/authorizations/auth-interceptor.service.ts
+++ src/app/services/authorizations/auth-interceptor.service.ts
@@ -1,11 +1,12 @@
 import {
   HttpEvent,
+  HttpEventType,
   HttpHandler,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -23,6 +24,14 @@ export class AuthInterceptorService implements HttpInterceptor {
     const modifiedRequest = req.clone({
       headers: req.headers.append('Auth', 'xyz'),
     });
-    return next.handle(modifiedRequest);
+    return next.handle(modifiedRequest).pipe(
+      tap((event) => {
+        console.log(event);
+        if (event.type === HttpEventType.Response) {
+          console.log('Response arrived, body data: ');
+          console.log(event.body);
+        }
+      })
+    );
   }
 }
