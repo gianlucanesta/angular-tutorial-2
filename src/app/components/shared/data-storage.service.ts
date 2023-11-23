274971db7baeb960ab643c5efbb4e76809d668ce
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-11-23 22:25:36 +0100

    Storing Recipes

--- src/app/components/shared/data-storage.service.ts
+++ src/app/components/shared/data-storage.service.ts
@@ -1,9 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private recipesService: RecipeService
+  ) {}
+
+  baseUrl =
+    'https://ng-course-recipe-book-6125d-default-rtdb.europe-west1.firebasedatabase.app/';
+  recipesRoute = '/recipes';
+  extJson = '.json';
+
+  storeRecipes() {
+    const recipes = this.recipesService.getRecipes();
+    return this.http
+      .put(this.baseUrl + this.recipesRoute + this.extJson, recipes)
+      .subscribe((response) => console.log(response));
+  }
 }
