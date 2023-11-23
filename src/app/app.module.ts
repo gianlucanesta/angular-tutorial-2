b27e87e5beb7f785b2d16c2b52dee64f719bf153
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-11-23 20:39:22 +0100

    Introducing Interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthInterceptorService } from './services/authorizations/auth-interceptor.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
