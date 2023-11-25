ebe3b7e8e40131c5f5a38c662fa861050702111e
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-11-25 15:51:35 +0100

    Sending the Signup Request

--- src/app/authentication/auth/auth.component.ts
+++ src/app/authentication/auth/auth.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../auth.service';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
@@ -9,12 +10,32 @@ import { NgForm } from '@angular/forms';
 export class AuthComponent {
   isLoginMode = true;
 
+  constructor(private authService: AuthService) {}
+
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
+    // console.log(form.value);
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLoginMode) {
+      //...
+    } else {
+      this.authService.signup(email, password).subscribe(
+        (resData) => {
+          console.log(resData);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
     form.reset();
   }
 }
