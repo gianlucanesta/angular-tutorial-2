0c501c7f04bfa60d373e2891f4e369a879789e59
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-12-04 22:07:46 +0100

    Setup App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './components/server/server.component';
-import { ServersComponent } from './components/servers/servers.component';
-import { WarningalertComponent } from './components/warningalert/warningalert.component';
-import { SuccessalertComponent } from './components/successalert/successalert.component';
-import { CockpitComponent } from './components/cockpit/cockpit.component';
-import { ServerElementComponent } from './components/server-element/server-element.component';
+import { SharedModule } from './components/shared/shared.module';
+import { DetailsComponent } from './components/welcome/details/details.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningalertComponent,
-    SuccessalertComponent,
-    CockpitComponent,
-    ServerElementComponent,
-  ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent, WelcomeComponent, DetailsComponent],
+  imports: [BrowserModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
