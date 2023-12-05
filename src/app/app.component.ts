384e683048ba7ab19740554ad6de57446710f6ad
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-12-05 22:26:12 +0100

    A Standalone Root Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @Component({
+  standalone: true,
+  imports: [WelcomeComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, WelcomeComponent],
+  declarations: [],
+  imports: [BrowserModule, AppComponent],
   providers: [],
-  bootstrap: [AppComponent],
 })
 export class AppModule {}
