5406ca6411febe9e0442ef651203d1180a7fb37d
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-12-05 22:49:45 +0100

    Routing with Standalone Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Route, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
+import { WelcomeComponent } from './components/welcome/welcome.component';
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: WelcomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./components/dashboard/dashboard-routing.module').then(
+        (mod) => mod.DashboardRoutingModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppComponent } from './app/app.component';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AnalyticsService } from './app/components/shared/analytics.service';
+import { importProvidersFrom } from '@angular/core';
+import { AppRoutingModule } from './app/app-routing.module';
 
 bootstrapApplication(AppComponent, {
-  providers: [AnalyticsService],
+  providers: [importProvidersFrom(AppRoutingModule)],
 });
