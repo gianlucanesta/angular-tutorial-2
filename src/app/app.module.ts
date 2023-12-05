ab1fdfab5cccd24cef8eb0d25c27c2127029d427
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-12-05 22:17:11 +0100

    Standalone Directives & Connecting Building Blocks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SharedModule } from './components/shared/shared.module';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { DetailsComponent } from './components/welcome/details/details.component';
 
 @NgModule({
   declarations: [AppComponent, WelcomeComponent],
-  imports: [BrowserModule, SharedModule, DetailsComponent],
+  imports: [BrowserModule, DetailsComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
