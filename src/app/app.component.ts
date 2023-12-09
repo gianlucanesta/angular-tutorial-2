e64739e9228ddc71e7dd2fd34a2adda9d650b175
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-12-09 20:11:13 +0100

    Setup App Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { DefaultComponent } from './components/default/default.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  standalone: true,
+  imports: [DefaultComponent],
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {}
