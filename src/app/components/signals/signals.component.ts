c50375dcebb95e3ad3819725375f9bd73b47bfb9
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-12-09 20:51:28 +0100

    Signal Updating: set(), update() & mutate()

--- src/app/components/signals/signals.component.ts
+++ src/app/components/signals/signals.component.ts
@@ -8,16 +8,23 @@ import { Component, signal } from '@angular/core';
   imports: [NgFor],
 })
 export class SignalsComponent {
-  actions: string[] = [];
+  // actions: string[] = [];
+  actions = signal<string[]>([]);
   counter = signal(0);
 
   increment() {
-    this.counter.update((oldCounter) => oldCounter + 1);
-    this.actions.push('INCREMENT');
+    // this.counter.update((oldCounter) => oldCounter + 1);
+    this.counter.set(this.counter() + 1);
+    this.actions.mutate((oldActions: any) => oldActions.push('INCREMENT'));
+    // this.actions.push('INCREMENT');
   }
 
   decrement() {
     this.counter.update((oldCounter) => oldCounter - 1);
-    this.actions.push('DECREMENT');
+    // this.counter.set(this.counter() - 1);
+
+    // this.actions.push('DECREMENT');
+    // this.actions.mutate((oldActions: any) => oldActions.push('DECREMENT'))
+    this.actions.update((oldActions) => [...oldActions, 'DECREMENT']);
   }
 }
