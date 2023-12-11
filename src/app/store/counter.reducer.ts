4bfec77a3a76352e6c83a3804939679437709022
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-12-11 21:34:48 +0100

    An Alternative Way Of Creating Reducers

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,3 +1,10 @@
+// import { counterReducer } from './counter.reducer';
 import { createReducer } from '@ngrx/store';
+
 const initialState = 0;
-export const counterReducer = createReducer(initialState);
+
+// export const counterReducer = createReducer(initialState);
+
+export function counterReducer(state = initialState) {
+  return state;
+}
