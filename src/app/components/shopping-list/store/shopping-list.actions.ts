8ca6aff60d6ba8351ab9dd6d0c97fde36f594c34
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2023-12-28 23:11:16 +0100

    Preparing Update & Delete Actions

--- src/app/components/shopping-list/store/shopping-list.actions.ts
+++ src/app/components/shopping-list/store/shopping-list.actions.ts
@@ -3,6 +3,8 @@ import { Ingredient } from '../../shared/ingredient.model';
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
 
 export class AddIngredient implements Action {
   readonly type = ADD_INGREDIENT;
@@ -13,7 +15,23 @@ export class AddIngredient implements Action {
 export class AddIngredients implements Action {
   readonly type = ADD_INGREDIENTS;
 
-  constructor(public payload: Ingredient[]) {}
+  constructor(public payload: { index: number; ingrediente: Ingredient }) {}
 }
 
-export type ShoppingListActions = AddIngredient | AddIngredients;
+export class DeleteIngredient implements Action {
+  readonly type = DELETE_INGREDIENT;
+
+  constructor(public payload: number) {}
+}
+
+export class UpdateIngredient implements Action {
+  readonly type = UPDATE_INGREDIENT;
+
+  constructor(public payload: {}) {}
+}
+
+export type ShoppingListActions =
+  | AddIngredient
+  | AddIngredients
+  | UpdateIngredient
+  | DeleteIngredient;
