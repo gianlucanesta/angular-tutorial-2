142debfbbee1ab8e3c4ebcc610a3a2ac96af11aa
Gianluca Nesta <gianluca.nesta@gmail.com>
2024-02-19 19:19:06 +0100

    Effects & Error Handling

--- src/app/authentication/store/auth.effect.ts
+++ src/app/authentication/store/auth.effect.ts
@@ -1,7 +1,46 @@
 import { Actions, ofType } from '@ngrx/effects';
 import * as AuthActions from './auth.actions';
+import { catchError, map, of, switchMap } from 'rxjs';
+import { AuthResponseData } from '../auth.service';
+import { HttpClient } from '@angular/common/http';
+
+export interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: string;
+}
 
 export class AuthEffects {
-  authlogin = this.actions$.pipe(ofType(AuthActions.LOGIN_START));
-  constructor(private actions$: Actions) {}
+  API_KEY: string = 'AIzaSyDwfiOSXlrP__97Vqt-2F8gMsVuK8HPyJc';
+
+  @Effect()
+  authlogin = this.actions$.pipe(
+    ofType(AuthActions.LOGIN_START),
+    switchMap((authData: AuthActions.LoginStart) => {
+      return this.http
+        .post<AuthResponseData>(
+          'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
+            this.API_KEY,
+          {
+            email: authData.payload.email,
+            password: authData.payload.password,
+            returnSecureToken: true,
+          }
+        )
+        .pipe(
+          catchError((error) => {
+            of();
+          }),
+          map((resData: any) => {})
+        );
+    })
+  );
+
+  constructor(private actions$: Actions, private http: HttpClient) {}
+}
+function Effect(): (target: AuthEffects, propertyKey: 'authlogin') => void {
+  throw new Error('Function not implemented.');
 }
