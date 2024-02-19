c60437db27d3c874bd4e642bfe8b3e8767febebf
Gianluca Nesta <gianluca.nesta@gmail.com>
2024-02-19 22:40:20 +0100

    Finishing the Login Effect

--- src/app/authentication/store/auth.effect.ts
+++ src/app/authentication/store/auth.effect.ts
@@ -1,8 +1,9 @@
-import { Actions, ofType } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import * as AuthActions from './auth.actions';
-import { catchError, map, of, switchMap } from 'rxjs';
+import { catchError, map, of, switchMap, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 export interface AuthResponseData {
   idToken: string;
@@ -16,41 +17,69 @@ export interface AuthResponseData {
 export class AuthEffects {
   API_KEY: string = 'AIzaSyDwfiOSXlrP__97Vqt-2F8gMsVuK8HPyJc';
 
-  @Effect()
-  authlogin = this.actions$.pipe(
-    ofType(AuthActions.LOGIN_START),
-    switchMap((authData: AuthActions.LoginStart) => {
-      return this.http
-        .post<AuthResponseData>(
-          'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
-            this.API_KEY,
-          {
-            email: authData.payload.email,
-            password: authData.payload.password,
-            returnSecureToken: true,
-          }
-        )
-        .pipe(
-          map((resData: any) => {
-            const expirationDate = new Date(
-              new Date().getTime() + resData.expiresIn * 1000
-            );
-            new AuthActions.Login({
-              email: resData.email,
-              userId: resData.userId,
-              token: resData.token,
-              expirationDate: resData.expirationDate,
-            });
-          }),
-          catchError((error) => {
-            return of();
-          })
-        );
-    })
+  authLogin = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.LOGIN_START),
+      switchMap((authData: AuthActions.LoginStart) => {
+        return this.http
+          .post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
+              this.API_KEY,
+            {
+              email: authData.payload.email,
+              password: authData.payload.password,
+              returnSecureToken: true,
+            }
+          )
+          .pipe(
+            map((resData) => {
+              const expirationDate = new Date(
+                new Date().getTime() + +resData.expiresIn * 1000
+              );
+              return new AuthActions.Login({
+                email: resData.email,
+                userId: resData.localId,
+                token: resData.idToken,
+                expirationDate: expirationDate,
+              });
+            }),
+            catchError((errorRes) => {
+              let errorMessage = 'An unknown error occurred!';
+              if (!errorRes.error || !errorRes.error.error) {
+                return of(new AuthActions.LoginFail(errorMessage));
+              }
+              switch (errorRes.error.error.message) {
+                case 'EMAIL_EXISTS':
+                  errorMessage = 'This email exists already';
+                  break;
+                case 'EMAIL_NOT_FOUND':
+                  errorMessage = 'This email does not exist.';
+                  break;
+                case 'INVALID_PASSWORD':
+                  errorMessage = 'This password is not correct.';
+                  break;
+              }
+              return of(new AuthActions.LoginFail(errorMessage));
+            })
+          );
+      })
+    )
   );
 
-  constructor(private actions$: Actions, private http: HttpClient) {}
-}
-function Effect(): (target: AuthEffects, propertyKey: 'authlogin') => void {
-  throw new Error('Function not implemented.');
+  authSuccess = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(AuthActions.LOGIN),
+        tap(() => {
+          this.router.navigate(['/']);
+        })
+      ),
+    { dispatch: false }
+  );
+
+  constructor(
+    private actions$: Actions,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 }
